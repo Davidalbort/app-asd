71de02b8a044a740070978d3d3e02ae58350fb5e
Roger David Alba Ortega <99060136+Davidalbort@users.noreply.github.com>
2022-12-14 22:55:13 -0500

    Feature(Components): Build components Modal for show error of authentication with its styles and logical

---
+++ components/Modal.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/modal.module.css'
+
+interface PropsModal {
+    handleClick: () => void
+}
+
+export const Modal = ({handleClick}: PropsModal) => {
+	return(
+		<div className={styles.container}>
+			<section className={styles.modal}>
+				<h3>User or password is wrong</h3>
+				<button onClick={handleClick}>Ok</button>
+			</section>
+		</div>
+	)
+}
